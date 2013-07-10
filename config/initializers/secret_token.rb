08bad4116d6843229b9800d24c7970b65e173d9e
earwerm <michael@earwerm.com>
2013-07-11 08:40:43 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uneekify::Application.config.secret_key_base = '7f78fa1e5d36d196dc42137af00f3aa4734a794078b0c44447b5ec4c8ae305d185c844863f335e1c35d0781913cb826e2d95a04d58067b409827e3bbf490afb1'
