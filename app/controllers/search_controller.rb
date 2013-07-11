950c06a4437312bf55ba5f98c422babd6adb76d1
earwerm <michael@earwerm.com>
2013-07-11 16:00:37 +1000

    no more JSON

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,16 +3,14 @@ class SearchController < ApplicationController
   end
 
   def return
-    #binding.pry
+
     @genre = params[:genre]
     @danceability = params[:danceability]
     @min_bpm = params[:min_bpm]
     @results_number = params[:results_number]
     @url = "http://developer.echonest.com/api/v4/song/search?api_key=9JJUAQS0ZBF2SK5VU&style=#{@genre}&min_danceability=#{@danceability}&min_tempo=#{@min_bpm}&results=#{@results_number}"
 
-
-    html = HTTParty.get(url)
-    @info = JSON(html)
-    #redirect_to(root_path)
+    response = HTTParty.get(@url) # Look ma, no JSON!
+    @songs = response['response']['songs']
   end
 end
